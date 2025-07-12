cba5b5ba9030ceec9930a24b96de7b81c0e9a5e2
Ken Tsukamoto <tsukaken0622@gmail.com>
2025-07-12 21:20:31 +0900

    ♻️ Footer,Link,Headline部のコンポネント化

---
+++ components/Headline.js
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function Headline(props) {
+  return (
+    <div>
+          <h1 className={styles.title}>{props.page} page</h1>
+          <Image
+            className={styles.logo}
+            src="/next.svg"
+            alt="Next.js logo"
+            width={180}
+            height={38}
+            priority
+          />
+          <ol>
+            <li>
+              Get started by editing <code>src/pages/{props.page}.js</code>.
+            </li>
+            <li>Save and see your changes instantly.</li>
+          </ol>
+      </div>
+  );
+}
