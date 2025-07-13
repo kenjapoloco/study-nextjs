2daa7fc53c05647690927682baa64b572e28bafd
Ken Tsukamoto <tsukaken0622@gmail.com>
2025-07-14 00:29:24 +0900

    ⚡️ useCallback,useEffectの第２引数の更新

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,7 @@ import classes from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const geistSans = Geist({variable: "--font-geist-sans",subsets: ["latin"],});
 const geistMono = Geist_Mono({variable: "--font-geist-mono",subsets: ["latin"],});
@@ -13,10 +13,11 @@ export default function Home() {
   const [count,setCount]= useState(1);
   // 配列の分割代入
   // 初期値は１
-  const handleClick = (e) => {
+  const handleClick = useCallback(() => {
+  if(count < 100) {
     setCount((count) => count + 1);
-
-  };
+    }
+  },[count]);
 
 
 
@@ -26,8 +27,7 @@ export default function Home() {
   return () => {
     document.body.style.backgroundColor = "";
   };
-},[]);
-
+},[]); 
 
   return (
     <div className={classes.container}>
