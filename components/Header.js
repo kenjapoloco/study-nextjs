df03d0e9c1beff3b1de4f83a7db3f125cbddcbe2
Ken Tsukamoto <tsukaken0622@gmail.com>
2025-07-13 19:10:45 +0900

    ✨ HeaderのLinkコンポーネント

---
+++ components/Header.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { Geist, Geist_Mono } from "next/font/google";
+import classes from "./Header.module.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export function Header() {
+  return (
+        <header className={classes.header}>
+         <Link href="/" className={classes.anchor}>Index</Link> 
+         <Link href="/about" className={classes.anchor}>About</Link>
+        </header>
+  );
+}
